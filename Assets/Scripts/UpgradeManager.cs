7342f8d7aa9aa043cee99d1a3a06843328a4cb86
Russell Brabers <brabers.russell@gmail.com>
2020-06-25 19:46:03 -0400

    First Dullios additions
    - Created Dullios scene
    - Added scripts:
       - AttributeDetails
       - LevelingNode
       - UpgradeManager

---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public AttributeDetails[] attDetails;
+
+    public Material defaultMat;
+    public Material redMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Increment(string att)
+    {
+        foreach(AttributeDetails attDetail in attDetails)
+        {
+            if(attDetail.attName == att)
+            {
+                if(attDetail.attValue < 4)
+                {
+                    attDetail.attValue++;
+                    attDetail.nodes[attDetail.attValue].GetComponent<MeshRenderer>().material = redMat;
+                }
+
+                break;
+            }
+        }
+    }
+
+    public void Decrement(string att)
+    {
+        foreach (AttributeDetails attDetail in attDetails)
+        {
+            if (attDetail.attName == att)
+            {
+                if (attDetail.attValue > attDetail.baseValue - 1)
+                {
+                    attDetail.nodes[attDetail.attValue].GetComponent<MeshRenderer>().material = defaultMat;
+                    attDetail.attValue--;
+                }
+
+                break;
+            }
+        }
+    }
+}
