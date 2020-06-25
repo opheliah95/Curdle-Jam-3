d03ccfccf88bf1676afb35ad9c43db8392834b18
Firas <firasaldabbas+git@gmail.com>
2020-06-25 19:08:54 -0400

    [PLAYER] Movement animator set up
    Just using placeholder assets, but its enough to work off of

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,30 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Vector2 movement;
     public Rigidbody2D rb;
+
+    public Animator animator;
     
-    // TODO: Controls for rock use
+    // TODO: Animations : Directional idle
     
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //animator.SetFloat("Horizontal", movement.x); 
+        animator.SetFloat("Horizontal", (movement != Vector2.zero ? movement.x : animator.GetFloat("Horizontal")));
+        //animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Vertical", (movement != Vector2.zero ? movement.y : animator.GetFloat("Vertical")));
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //transform.localScale = new Vector2((movement.x >= 0? 1: -1), 1);
+        transform.localScale = new Vector2((animator.GetFloat("Horizontal") < 0? -1 : 1), 1);
     }
 
     private void FixedUpdate()
--- Assets/Scripts/PlayerViolence.cs
+++ Assets/Scripts/PlayerViolence.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class PlayerViolence : MonoBehaviour
 {
-    // TODO: Pull stats from Rock
+    // TODO: Pull stats from Rock - swing speed, aoe, throw area, throw speed
     // TODO: Fill in that skeleton
+    // TODO: Animations and timers
+
+    public bool isThrowing;
+    public bool isSwinging;
+    public float cooldown = 0.25f;
     
     void Update()
     {
-        
+        // Input
+        // LMB
+        // Single click because you can't spam-throw
+        if (Input.GetMouseButtonDown(0))
+        {
+            RockThrow();
+        }
+        // RMB
+        // Down for repeated swinging
+        if (Input.GetMouseButton(1))
+        {
+            RockSmash();
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        // Using input
     }
 
     void RockThrow()
     {
-
+        print("I throw the stone.");
     }
 
     void RockSmash()
     {
-
+        print("Swing and a....");
     }
 }
