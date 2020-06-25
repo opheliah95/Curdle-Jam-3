48da911bd3a676bf939d61ec758aa5e78e68b39a
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-25 17:33:57 +0100

    [MENU] Added menu script [SCRIPT] Added a folder for script

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // start game 
+    public void StartGame()
+    {
+        Debug.Log("Level start");
+    }
+
+    // load options
+    public void ShowOptions()
+    {
+        Debug.Log("Loading options");
+    }
+
+    // quit games
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
