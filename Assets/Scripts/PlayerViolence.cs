1c3ccba0060e6708267184f80fcc368d60d147ee
Firas <firasaldabbas+git@gmail.com>
2020-06-25 16:31:32 -0400

    [PLAYER] Set up skeleton for rock usage

---
+++ Assets/Scripts/PlayerViolence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerViolence : MonoBehaviour
+{
+    // TODO: Pull stats from Rock
+    // TODO: Fill in that skeleton
+    
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void RockThrow()
+    {
+
+    }
+
+    void RockSmash()
+    {
+
+    }
+}
