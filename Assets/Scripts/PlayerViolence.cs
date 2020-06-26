20a2b282cd1f8229a13b87d88d627a4313f1707c
Firas <firasaldabbas+git@gmail.com>
2020-06-26 17:41:13 -0400

    [ANIMATION[ Player throw animations and transitions are in

--- Assets/Scripts/PlayerViolence.cs
+++ Assets/Scripts/PlayerViolence.cs
@@ -28,30 +28,38 @@ public class PlayerViolence : MonoBehaviour
 
     void Update()
     {
-        // Input
-        // RMB
-        // Single click because you can't spam-throw
-        if (Input.GetMouseButtonDown(1))
+        if (hasRock)
         {
-            
-        }
-        // LMB
-        // Down for repeated swinging
-        if (Input.GetMouseButton(0))
-        {
-            isAttacking = true;
-        }
-        else
-        {
-            isAttacking = false;
+            // There's gotta be a simpler way, but fuck it, mad at git, can't be bothered.
+
+            // Input
+            // RMB
+            // Single click because you can't spam-throw
+            // Also throw will take precedence.
+            if (Input.GetMouseButtonDown(1))
+            {
+                isThrowing = true;
+                isAttacking = false;
+            }
+            // LMB
+            // Down for repeated swinging
+            else if (Input.GetMouseButton(0))
+            {
+                isAttacking = true;
+                isThrowing = false;
+            }
+            else
+            {
+                isAttacking = false;
+                isThrowing = false;
+            }
         }
     }
 
     private void FixedUpdate()
     {
+        animator.SetBool("IsThrowing", isThrowing);
         animator.SetBool("IsAttacking", isAttacking);
-        // handled in animator
-        //rockSmashCollider.enabled = isAttacking;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
