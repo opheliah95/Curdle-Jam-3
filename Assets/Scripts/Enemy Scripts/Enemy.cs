aad2e34bc5c0c011f45fc69dea66fc20c9ef2062
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-28 00:18:16 +0100

    [TILEMAP] Loaded sample tilemaps [AI] Started bird scripts

---
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public void Idle()
+    {
+
+    }
+
+    public void Flee()
+    {
+
+    }
+
+
+}
