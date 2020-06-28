2fac73192a4e6cec85481276f5194b3d8037ea0d
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-28 02:53:12 +0100

    [AI] Fixed escaping script, however, fliping issue occurs

--- Assets/Scripts/Enemy Scripts/AIControls/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/AIControls/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     [Tooltip("how far enemy can see")]
     public float detectionRadius = 3f;
     [Tooltip("how fast they can run")]
-    public float speed = 2f, escapingRadius = 20f;
+    public float speed = 2f, escapingSpeed = 10f, escapingRadius = 20f;
 
     [SerializeField]
     Vector3 startingPos;
@@ -20,6 +20,8 @@ public class Enemy : MonoBehaviour
 
     [SerializeField]
     bool flipLeft = false;
+    [SerializeField]
+    bool isEscaping = false;
 
     protected void Start()
     {
@@ -30,19 +32,23 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        if (Escape())
-            return;
-
-        moveToPosition(roamingPos);
-        // reached position, then restart again
-        if (Vector3.Distance(transform.position, roamingPos) <= 0.1f)
+        Escape();
+        if (!isEscaping)
         {
-            // resetart whole movement and move to positon
-            startingPos = transform.position;
-            roamingPos = randomMovement();
-            moveToPosition(roamingPos);
+            moveToPosition(roamingPos, speed);
+            // reached position, then restart again
+            if (Vector3.Distance(transform.position, roamingPos) <= 0.1f)
+            {
+                // resetart whole movement and move to positon
+                startingPos = transform.position;
+                roamingPos = randomMovement();
+                moveToPosition(roamingPos, speed);
+            }
+
         }
+  
     }
+
     public void Idle()
     {
       
@@ -64,41 +70,52 @@ public class Enemy : MonoBehaviour
     // fine tune so that enemy only move in one direction
     protected Vector3 randomMovement()
     {
-        return MyUtil.Util.GetSingleDir() + startingPos;
+        return Util.GetSingleDir() + startingPos;
 
     }
 
-    public void moveToPosition(Vector3 pos)
+    public void moveToPosition(Vector3 pos, float movingSpeed)
     {
-        float step = speed * Time.deltaTime;
+        float step = movingSpeed * Time.deltaTime;
         // move sprite towards the target location
         transform.position = Vector2.MoveTowards(transform.position, pos, step);
 
-        // flip sprite
+        // flip sprite ---side only
         if ((pos.x < 0 && !flipLeft) || (pos.x > 0 && flipLeft))
         {
             flipEnemySide();
         }
 
+        // flip sprites ---up/down
+
+        Debug.Log("moving");
     }
 
     // check escape
-    public bool Escape()
+    public void Escape()
     {
         Vector3 playerPos = playerTransform.position;
-        
+
+
         // check if player in radius
-        if (Vector3.Distance(transform.position, playerPos) < detectionRadius)
+        if ((Vector3.Distance(transform.position, playerPos) < detectionRadius))
         {
-            Debug.Log("escaping");
+          
             Vector3 dirToPlayer = transform.position - playerPos;
             Vector3 newPos = dirToPlayer.normalized * escapingRadius + transform.position;
-            moveToPosition(newPos);
-            return true;
+            moveToPosition(newPos, escapingSpeed);
+            isEscaping = true;
+
+        }
+        else
+        {
+            isEscaping = false;
+            return;
         
         }
+        
 
-        return false;
+        
     }
 
     // check if enemy is stuck in wall
